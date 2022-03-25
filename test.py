e101a1d83fca84095db5cfe07676ae81c3237ebe
kwokkayan <kwokkayan0712@gmail.com>
2022-03-25 16:35:44 +0800

    added buffer

--- test.py
+++ test.py
@@ -203,12 +203,12 @@ class VoiceChannelCommand(commands.Cog):
                             first["urls"] = urls
                             first["nextPageToken"] = token
                     #spawn process
-                    self.yt_process = subprocess.Popen(getYTDLArgs(first["urls"][0]), stdout=subprocess.PIPE)
+                    self.yt_process = subprocess.Popen(getYTDLArgs(first["urls"][0]), bufsize=3840, stdout=subprocess.PIPE)
                     self.source = discord.FFmpegOpusAudio(self.yt_process.stdout, pipe=True, options=FFMPEG_OPTIONS)
                     #play audio
                     common_vc.play(self.source, after=cleanup)
                 # playing song
-                self.yt_process = subprocess.Popen(getYTDLArgs(self.queue.getFirstOfQueue()["urls"][0]), stdout=subprocess.PIPE)
+                self.yt_process = subprocess.Popen(getYTDLArgs(self.queue.getFirstOfQueue()["urls"][0]), bufsize=3840, stdout=subprocess.PIPE)
                 self.source = discord.FFmpegOpusAudio(self.yt_process.stdout, pipe=True, options=FFMPEG_OPTIONS)
                 common_vc.play(self.source, after=cleanup)
             except discord.ClientException:
@@ -276,151 +276,6 @@ class VoiceChannelCommand(commands.Cog):
         for embed in embeds:
             await ctx.send(embed=embed)
 
-# class MyClient(discord.Client):
-#     yt_process = None
-#     source = None
-#     playing_list = False
-#     playlistIds = []
-#     queue = YTSongQueue() # if all results --> FUTURE: CREATE QUEUE CLASS BECAUSE THIS IS BAD
-
-#     def isInGuildVC(self, guildVCs):
-#         for vc in self.voice_clients:
-#             for g_vc in guildVCs:
-#                 if vc == g_vc:
-#                    return True
-#         return False 
-
-#     def getCommonVC(self, user):
-#         for vc in self.voice_clients:
-#             if vc == user:
-#                 return vc
-#         return None
-
-#     def killPlayingProcess(self, vc):
-#         self.yt_process.kill()
-#         vc.stop()
-#         self.yt_process.wait()
-
-#     async def on_ready(self):
-#         print('Logged on as {0}!'.format(self.user))
-    
-#     async def on_message(self, message):
-#         #print('Message from {0.author}: {0.content}'.format(message))
-#         command = message.content.split(" ")
-#         if (command[0] == "!help"):
-#             pass
-#         # Voice related commands
-#         voiceClient = self.getCommonVC(message.author.voice)
-#         if (command[0] == "!connect"): # connect logic
-#             if (message.author.voice == None):
-#                 await message.channel.send("Please connect to a voice channel")
-#                 return
-#             if (voiceClient):
-#                 await self.voiceClient.move_to(message.author.voice.channel)
-#             else:
-#                 await message.author.voice.channel.connect()
-#             await message.channel.send("Connected to " + str(message.author.voice.channel))
-#         if (voiceClient):
-#             if (command[0] == "!disconnect"): # disconnect logic
-#                 self.queue.emptyQueue()
-#                 if (voiceClient.is_playing() or voiceClient.is_paused()):
-#                     self.killPlayingProcess(voiceClient)
-#                 await voiceClient.disconnect()
-#             if (command[0] == "!play"): # play music logic --> youtube supported NEED TO ADD PLAYLIST
-#                 if (len(command) != 2): # add options
-#                     await message.channel.send("Usage: !play youtubeURL")
-#                     return
-#                 #add to queue
-#                 query_dict = parse_qs(urlparse(command[1]).query)
-#                 if (query_dict.get("list")):
-#                     plId = query_dict.get("list")[0]
-#                     self.playing_list = True
-#                     urls, token = parseVideoUrls(fetchPlaylistData(plId)) 
-#                     self.queue.enqueue({"listId" : plId, "urls": urls, "nextPageToken": token})
-#                 else:
-#                     self.queue.enqueue({"listId" : None, "urls": [command[1]], "nextPageToken": None})
-#                 # return message 
-#                 if (voiceClient.is_playing() or voiceClient.is_paused()):
-#                     await message.channel.send("Enqueued Song/List")
-#                     return
-#                 else:
-#                     await message.channel.send("Playing Song")
-#                     try:
-#                         # CLEAN UP SUBROUTINE
-#                         def cleanup(error): # ALWAYS KILL THE PROCESS BEFORE KILLING THE VOICECLIENT
-#                             if (self.queue.isEmpty()):
-#                                 return
-#                             self.yt_process.kill()
-#                             self.source.cleanup()
-#                             first = self.queue.getFirstOfQueue()
-#                             first["urls"].pop(0) # remove the song just played
-#                             if (len(first["urls"]) == 0): # no more urls
-#                                 if (first["nextPageToken"] == None): # check next page
-#                                     self.queue.dequeue() # remove the item
-#                                     if self.queue.isEmpty(): # empty queue
-#                                         return
-#                                     else:
-#                                         first = self.queue.getFirstOfQueue()
-#                                 else:
-#                                     urls, token = parseVideoUrls(fetchPlaylistData(first["listId"], nextPage=first["nextPageToken"])) 
-#                                     first["urls"] = urls
-#                                     first["nextPageToken"] = token
-#                             #spawn process
-#                             self.yt_process = subprocess.Popen(getYTDLArgs(first["urls"][0]), stdout=subprocess.PIPE)
-#                             self.source = discord.FFmpegOpusAudio(self.yt_process.stdout, pipe=True, options=FFMPEG_OPTIONS)
-#                             #play audio
-#                             voiceClient.play(self.source, after=cleanup)
-#                         # playing song
-#                         self.yt_process = subprocess.Popen(getYTDLArgs(self.queue.getFirstOfQueue()["urls"][0]), stdout=subprocess.PIPE)
-#                         self.source = discord.FFmpegOpusAudio(self.yt_process.stdout, pipe=True, options=FFMPEG_OPTIONS)
-#                         voiceClient.play(self.source, after=cleanup)
-#                     except discord.ClientException:
-#                         await message.channel.send("Error: playback error!")
-#                         return
-#             if (command[0] == "!stop"): # stop music logic
-#                 if (voiceClient.is_playing() or voiceClient.is_paused()):
-#                     self.queue.emptyQueue()
-#                     self.killPlayingProcess(voiceClient)
-#                     await message.channel.send("Player stopped!")
-#             if (command[0] == "!pause"): # pause music logic
-#                 if (voiceClient.is_playing()):
-#                     voiceClient.pause()
-#                     await message.channel.send("Song paused!")
-#             if (command[0] == "!resume"): # resume music logic
-#                 if (voiceClient.is_paused()):
-#                     voiceClient.resume()
-#                     await message.channel.send("Song resumed!")
-#             if (command[0] == "!skip"):
-#                 if (voiceClient.is_playing() or voiceClient.is_paused()):
-#                     self.killPlayingProcess(voiceClient)
-#                     await message.channel.send("Song skiped! Playing next song if possible...")
-#                 else:
-#                     await message.channel.send("No Songs are playing!")
-#             if (command[0] == "!lq" or command[0] == "!listqueue"):
-#                 if len(command) != 1 and len(command) != 2:
-#                     await message.channel.send("Usage: !listqueue [num]")
-#                     return
-#                 if self.queue.isEmpty():
-#                     await message.channel.send("Song queue is empty!")
-#                     return
-#                 prntnum = 5
-#                 if (len(command) == 2):
-#                     prntnum = int(command[1])
-#                 song_urls = []
-#                 queuepos = 0
-#                 while prntnum > 0:
-#                     queueData = self.queue.getNthOfQueue(queuepos)
-#                     if (queueData == None):
-#                         break
-#                     song_urls += queueData["urls"][:prntnum]
-#                     prntnum -= len(queueData["urls"])
-#                     queuepos += 1
-#                 print(song_urls)
-#                 embeds = parseSongData(fetchSongData(song_urls))
-#                 for embed in embeds:
-#                     await message.channel.send(embed=embed)
-#         #loop function
-#         print(self.voice_clients, voiceClient, self.queue)
 client = commands.Bot(command_prefix="!")
 client.add_cog(VoiceChannelCommand(client))
 client.run(env.str("BOT_SECRET"))
